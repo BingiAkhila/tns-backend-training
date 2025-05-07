954412746906aa76bd84fd78de6b8f9ea1922db0
Akhila <Akhila@DESKTOP-THQMP3T>
2025-05-07 19:20:52 +0530

    add

---
+++ tns/src/com/tnsif/demoString/StringDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.demoString;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+	 StringBuffer s1=new StringBuffer();
+	 System.out.println(s1.capacity());
+	 StringBuffer s2=new StringBuffer("TNSIF");
+	 System.out.println(s2.capacity());
+
+	}
+
+}
