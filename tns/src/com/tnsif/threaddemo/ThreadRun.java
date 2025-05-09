fb8da6dcfc31ca83c3b57964493604fae2042150
Akhila <Akhila@DESKTOP-THQMP3T>
2025-05-09 21:23:56 +0530

    thread

---
+++ tns/src/com/tnsif/threaddemo/ThreadRun.java
@@ -0,0 +1,22 @@
+package com.tnsif.threaddemo;
+	//demo for runnable interface
+	public class ThreadRun extends ThreadDemo implements Runnable {
+		String name;
+		public ThreadRun(String name) {
+			this.name=name;
+		}
+		@Override
+		public void run() {
+	     for(int i=0;i<5;i++) {
+	   	  System.out.println("The current thread"+name);
+	   	  try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	     }
+		}
+	}
+
+
